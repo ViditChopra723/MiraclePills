1f8d3bd1f49e3b1e97266afe6e5c38a68d7d2d9c
Vidit Chopra <viditchopra@outlook.com>
2017-03-06 14:47:38 -0800

    Added two text fields, a UI picker for states, and button to pick states

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.view.backgroundColor = UIColor.purple            change bg color
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +32,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPrsd(_ sender: Any) {
+        statePicker.isHidden = false
+    }
 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+       return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return states.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        
+        statePicker.isHidden = true
+        
+    }
+    
 }
 
